9955b3036348d677844aa8cdc76da1f8ba9f443d
Kapil <kapilishwarkar2004@gmail.com>
2026-02-09 20:44:49 +0530

    fix: update CORS configuration for production frontend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
   origin: ["http://localhost:5173",
-  "https://x-twitter-fullstack.vercel.app"],
+  "https://x-twitter-fullstack.vercel.app",
+  "https://x-twitter-fullstack-git-main-kapil-ishwarkars-projects.vercel.app",
+  "https://x-twitter-fullstack-7i18z7r19-kapil-ishwarkars-projects.vercel.app"],
   credentials: true
 }
 app.use(cors(corsOptions))
