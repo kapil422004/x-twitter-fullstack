f29d837c9e7a054c2df628a13f8fdd9b8addf519
Kapil <kapilishwarkar2004@gmail.com>
2026-02-09 20:34:40 +0530

    fix: update CORS configuration for production frontend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ dbconnect();
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173",
+  "https://x-twitter-fullstack.onrender.com"],
   credentials: true
 }
 app.use(cors(corsOptions))
