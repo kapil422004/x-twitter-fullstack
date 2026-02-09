15cf5d203f0305ba418e90beb20a2e9f6971a49f
Kapil <kapilishwarkar2004@gmail.com>
2026-02-09 23:46:00 +0530

    fix: update CORS configuration for production frontend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dbconnect from "./config/dbconnect.js";
 import cookieParser from "cookie-parser";
 import router from "./routes/authRoutes.js";
 import tweetRouter from "./routes/tweetRoutes.js";
-import cors from "cors"
+import cors from "cors";
 const app = express();
 
 const port = process.env.PORT || 8080;
@@ -13,21 +13,21 @@ dbconnect();
 
 //middleware
 const corsOptions = {
-  origin: ["http://localhost:5173",
-  "https://x-twitter-fullstack.vercel.app",
-  "https://x-twitter-fullstack-git-main-kapil-ishwarkars-projects.vercel.app",
-  "https://x-twitter-fullstack-7i18z7r19-kapil-ishwarkars-projects.vercel.app"],
-  credentials: true
-}
-app.use(cors(corsOptions))
+  origin: [
+    "http://localhost:5173",
+    "https://x-twitter-fullstack.vercel.app",
+    "https://x-twitter-fullstack-git-main-kapil-ishwarkars-projects.vercel.app",
+    "https://x-twitter-fullstack-j6y0h9zbg-kapil-ishwarkars-projects.vercel.app",
+  ],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-
 //api
 app.use("/api/v1/user", router);
-app.use("/api/v1/tweet", tweetRouter)
-
+app.use("/api/v1/tweet", tweetRouter);
 
 app.listen(port, () => {
   console.log(`server is live at ${port}`);
